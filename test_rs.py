6ca8b821fb8317678ea95db09908701d2562483a
gruschak <gruschak@users.noreply.github.com>
2019-12-31 01:00:19 +0700

    Update test_rs.py

--- test_rs.py
+++ test_rs.py
@@ -2,8 +2,6 @@ import requests
 import zeep
 from zeep.client import Client
 
-payload = {'KEY1': '451a268b4059ff9161bdc51ad8dda536', 'KEY2': 'd0c698e43dab88680445971a4ec9db98', 'TEXT': '333114'}
-
 # RPC style soap service
 client = Client('http://api.rossko.ru/service/GetSearch')
-print(client.service.GetSearch(KEY1='451a268b4059ff9161bdc51ad8dda536', KEY2='d0c698e43dab88680445971a4ec9db98', TEXT='333114'))
+print(client.service.GetSearch(KEY1='451a268bwr59ff9161bdc51ad8dda536', KEY2='z0c68e43dab88680445971a4ec9db98', TEXT='333114'))
