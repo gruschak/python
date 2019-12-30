418bb46c1fd722d0d66037d438e840125211d41c
gruschak <gruschak@users.noreply.github.com>
2019-12-31 03:04:00 +0700

    Update test_rs.py

--- test_rs.py
+++ test_rs.py
@@ -5,7 +5,7 @@ from zeep.client import Client
 
 # RPC style soap service
 client = Client('http://api.rossko.ru/service/GetSearch')
-response = client.service.GetSearch(KEY1='451a268b4059ff9161bdc51ad8dda536', KEY2='d0c698e43dab88680445971a4ec9db98', TEXT='333144')
+response = client.service.GetSearch(KEY1='451a268b4059ff9161bwrasc51ad8dda536', KEY2='dfg0c698e43dab88680445971a4ec9db98', TEXT='333144')
 print("len(response.PartsList) = ", len(response.PartsList))
 print("len(response.PartsList.Part) = ", len(response.PartsList.Part))
 i = 0
