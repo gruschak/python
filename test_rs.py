f860277599fa4966309fd38eb86388d0deba3494
gruschak <gruschak@users.noreply.github.com>
2020-01-01 02:36:29 +0700

    Update test_rs.py

--- test_rs.py
+++ test_rs.py
@@ -3,14 +3,22 @@ import requests
 import zeep
 from zeep.client import Client
 
+Rossko = {'HST145': 'RS', 'HST40': 'RSN'}
+
 # RPC style soap service
 client = Client('http://api.rossko.ru/service/GetSearch')
-response = client.service.GetSearch(KEY1='451a268b4059ff9161bwrasc51ad8dda536', KEY2='dfg0c698e43dab88680445971a4ec9db98', TEXT='333144')
+response = client.service.GetSearch(KEY1='', KEY2='', TEXT='LAC105')
 print("len(response.PartsList) = ", len(response.PartsList))
 print("len(response.PartsList.Part) = ", len(response.PartsList.Part))
 i = 0
 for part in response.PartsList.Part:
     if part.brand and part.partnumber and part.name:
+        i += 1
         # print(i, " ", part)
         print(i, ':  ', part.brand + ' # ' + part.partnumber + ' : ' + part.name)
-        i += 1
+        if part.stocks:
+            print(type(part.stocks.stock)) # class 'list'
+            for ps in part.stocks.stock :
+                if ps.id in Rossko:
+                    print(Rossko[ps.id], ps.price, ps.count)
+
