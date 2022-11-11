305411a4eb019e02d7d9ff92180e3aa315cdd1b9
Максим Грущак <m.grushchak@bars-open.ru>
2022-11-11 03:55:10 +0300

    Add comments and doc strings

--- graph/graph_cycles.py
+++ graph/graph_cycles.py
@@ -11,9 +11,16 @@ class Graph(dict):
         super().__init__()
 
     def neighbours(self, vertex) -> list:
+        """ """
         return self.get(vertex) or []
 
     def read(self, file_name):
+        """ Read a graph from the text file.
+        A data in the first line must be
+        a number of graph edges to read.
+        The rest lines must have pairs of vertexes like
+        A B - that means an arrow from A to B.
+        """
         with open(file_name) as f:
             total_edges = int(f.readline())
             for _ in range(total_edges):
@@ -24,42 +31,44 @@ class Graph(dict):
                     self[a] = [b]
 
     def vertexes(self) -> set:
+        """ Return all vertexes of the graph """
         all_vertexes = set()
         for k, v in self.items():
             all_vertexes |= {k} | set(v)
         return all_vertexes
 
 
-class StateColour(Enum):
+class StateColor(Enum):
     WHITE = 'Not visited yet'
     GRAY = 'Currently in process'
     BLACK = 'Finished (visited Ok)'
 
 
 def check_graph_for_cycles(data_file) -> tuple[Graph, bool]:
+    """ """
+    color: dict[Any, StateColor] = {}
 
-    color: dict[Any, StateColour] = {}
-
-    def paint_all_vertexes(g: Graph):
+    def paint_all_vertexes(g: Graph, colour: StateColor):
         """ """
         for vertex in g.vertexes():
-            color[vertex] = StateColour.WHITE
+            color[vertex] = colour
 
-    def cycle_dfs(g: Graph, initial_vertex: int = None) -> bool:
+    def cycle_dfs(g: Graph, initial_vertex=None) -> bool:
         """ Depth-first search for a cycle in the graph starting from the given vertex
         :param g: a set of key-values pairs like {vertex: list_of_neighbour_vertexes}
         :param initial_vertex: a vertex to start from
         :return: True if any cycle is found else False
         """
-        color[initial_vertex] = StateColour.GRAY
+        color[initial_vertex] = StateColor.GRAY
 
         for neighbour in (g.vertexes() if initial_vertex is None else g.neighbours(initial_vertex)):
-            if color[neighbour] is StateColour.GRAY:
+
+            if color[neighbour] is StateColor.GRAY:
                 return True
-            if (color[neighbour] is StateColour.WHITE) and cycle_dfs(g, neighbour):
+            if (color[neighbour] is StateColor.WHITE) and cycle_dfs(g, neighbour):
                 return True
 
-        color[initial_vertex] = StateColour.BLACK
+        color[initial_vertex] = StateColor.BLACK
         return False
 
     #
@@ -67,7 +76,7 @@ def check_graph_for_cycles(data_file) -> tuple[Graph, bool]:
     #
     graph = Graph()
     graph.read(file_name=data_file)
-    paint_all_vertexes(graph)
+    paint_all_vertexes(graph, StateColor.WHITE)
     return graph, cycle_dfs(graph)
 
 
